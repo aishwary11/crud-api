abc8898ae31b9972fe3c4678759fb9d33e4923ae
Aishwary Shah <aishwary.shah@corp.dealmoney.in>
2019-12-31 17:52:58 +0530

    update 31-12-2019

--- src/app/component/instagram/instagram.component.ts
+++ src/app/component/instagram/instagram.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Instafeed } from 'instafeed.js';
 @Component({
   selector: 'instagram',
   templateUrl: './instagram.component.html',
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class InstagramComponent implements OnInit {
   constructor() {
   // constructor(public insta: Instafeed) {
-    // var feed = new insta({
-    //   get: 'user',
-    //   userId: '13775857179',
-    //   resolution: 'standard_resolution',
-    //   accessToken: '13775857179.1677ed0.08af12ebd0764ef5a7706bc3831fb7e3',
-    //   sortBy: 'most-recent',
-    //   template: '<div class="gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-    //   limit: 15,
-    // });
-    // feed.run();
+  //   var feed = new insta({
+  //     get: 'user',
+  //     userId: '13775857179',
+  //     resolution: 'standard_resolution',
+  //     accessToken: '13775857179.1677ed0.08af12ebd0764ef5a7706bc3831fb7e3',
+  //     sortBy: 'most-recent',
+  //     template: '<div class="gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+  //     limit: 15,
+  //   });
+  //   feed.run();
   }
 
   ngOnInit() {
